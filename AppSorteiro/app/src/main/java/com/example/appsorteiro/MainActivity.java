4f8f85b1c10ee639bb0655aad771b3e38c6f7c9c
Henrique Fernandes Cassoli <80260069+HenriqueCassoli@users.noreply.github.com>
2024-12-26 05:57:27 -0300

    Add files via upload

---
+++ AppSorteiro/app/src/main/java/com/example/appsorteiro/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.appsorteiro;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView label;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn = (Button) findViewById(R.id.btn2);
+        label = (TextView) findViewById(R.id.texto2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = new Random().nextInt(10) + 1;
+                label.setText("Resultado 1");
+            }
+        });
+        ;
+    }
+}
